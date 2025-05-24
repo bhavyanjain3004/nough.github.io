a57ec8a878ba5b2f948b1ceaf9b322753807b469
Bhavya jain <bhavyajain3004@gmail.com>
2025-05-25 00:12:01 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { ClerkProvider } from '@clerk/nextjs'
  
 const font = DM_Sans({ subsets: ['latin'] })
 
@@ -16,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+  >
     <html lang="en">
       <body className={font.className}>
+      
+
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -28,5 +34,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   )
 }
